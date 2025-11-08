0e2141971e2e8716e9ea03c8be1d18c59ce9ebbe
Mike Melancon <michmela44@gmail.com>
2025-11-07 21:59:33 -0600

    fix version number

--- src/components/App/version.js
+++ src/components/App/version.js
@@ -17,7 +17,7 @@
 */
 import { h } from "preact"
 import { webUIbuild } from "../../targets"
-export const webUIversion = "3.0.0-6-beta"
+export const webUIversion = "3.0.0.6"
 export const Esp3dVersion = () => (
     <span>
         {webUIversion}.{webUIbuild}
